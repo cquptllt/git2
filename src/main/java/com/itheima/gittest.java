fc536768a13cde79b3295cc0ec8405dd438f9694
cquptllt <143787503+cquptllt@users.noreply.github.com>
2023-09-01 18:08:48 +0800

    hot-fix commit2

--- src/main/java/com/itheima/gittest.java
+++ src/main/java/com/itheima/gittest.java
@@ -4,9 +4,11 @@ public class gittest {
     public static void main(String[] args) {
 
         System.out.println("hello git");
-thir        System.out.println("hello git2");
+     System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello git4");
+        System.out.println("hot-fix test!");
+
 
     }
 }
