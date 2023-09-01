0a2799f06c884bf1059d680dc80e47fe2b0e79be
cquptllt <143787503+cquptllt@users.noreply.github.com>
2023-09-01 17:55:55 +0800

    third commit

--- src/main/java/com/itheima/gittest.java
+++ src/main/java/com/itheima/gittest.java
@@ -4,6 +4,8 @@ public class gittest {
     public static void main(String[] args) {
 
         System.out.println("hello git");
+thir        System.out.println("hello git2");
+        System.out.println("hello git3");
 
     }
 }
